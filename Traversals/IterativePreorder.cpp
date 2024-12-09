a7818efcad2cd4becafb8af1367b5a1aafcfe292
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-12-09 20:06:36 +0530

    Iterative preorder traversal

---
+++ Traversals/IterativePreorder.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        
+
+        vector<int> ans;
+
+        if(root == NULL) return ans;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()){
+            TreeNode* node = st.top();
+            st.pop();
+            if(node->right){
+                st.push(node->right);
+            }
+            if(node->left){
+                st.push(node->left);
+            }
+
+            ans.push_back(node->val);
+        }
+
+        return ans;
+
+    }
+};
