65b49c0cd010deafabc7a5af7fb1c022d22b87a9
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-12-10 20:02:19 +0530

    Added optimised approach for the height is balanced or not

--- InterviewQuestions/HeightOfTree.cpp
+++ InterviewQuestions/HeightOfTree.cpp
@@ -19,3 +19,28 @@ public:
         return 1 + max(maxDepth(root->left),maxDepth(root->right));
     }
 };
+
+
+// optimised approach
+
+class Solution {
+public:
+    int height(TreeNode* node){
+
+        if(node == NULL) return 0;
+
+        int lh = height(node->left);
+        if(lh == -1) return -1;
+        int rh = height(node->right);
+        if(rh == -1) return -1;
+
+        if(abs(lh-rh) > 1) return -1;
+
+        return 1 + max(lh,rh);
+    }
+
+    bool isBalanced(TreeNode* root) {
+        
+        return height(root) != -1;
+    }
+};
