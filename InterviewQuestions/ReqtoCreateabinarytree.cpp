e100d91265b55a7c44cb6324dc94d5d814f6cc36
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-12-14 10:17:13 +0530

    Req needed to construct a binary tree

---
+++ InterviewQuestions/ReqtoCreateabinarytree.cpp
@@ -0,0 +1,40 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Note:- Basically we must need a inorder to construct a binary tree
+
+class Solution
+{
+public:
+    bool isPossible(int a,int b)
+    {
+        // 2 means it is inorder
+        
+        if( a == b) return false;
+        
+        if(a == 2 || b == 2) return true;
+        
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b; 
+        cin>>a>>b;
+        Solution obj;
+        bool ans=obj.isPossible(a,b);
+        cout<<ans<<"\n";
+    
+cout << "~" << "\n";
+}
+}
+// } Driver Code Ends
